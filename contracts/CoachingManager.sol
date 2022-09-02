9d16c10bcf71e9b00653eabdbe5cc7de8292f207
bugra <bugrahan.duran@anywai.ch>
2022-09-02 10:06:50 +0300

    docs: Added docstring

--- contracts/CoachingManager.sol
+++ contracts/CoachingManager.sol
@@ -22,13 +22,16 @@ abstract contract CoachingManager is BasePlatform {
     mapping(uint => bool) leaderDistributed;
 
     function buyCoaching(address _coach, uint _teamId) external {
-        /// @dev Should be updated
+        /// @notice Allows users to buy coaching service. Please note that coaching fees may change
+        /// for the same coach depending on the team that a coach belongs to. 
+        /// @param _coach The address of the coach that a user want to buy service from
+        /// @param _teamId The ID of the team that the coach belongs to.
         require(ikyc.getKYC(msg.sender), "You are not KYCed");
+        require(!ikyc.getBan(_coach), "Coach is banned");
         require(
             teamMembers[_teamId][_coach],
             "Address is not registered as a coach in this team."
         );
-        require(!ikyc.getBan(_coach), "Coach is banned");
         foundationBalance +=
             (coachingFees[_teamId][_coach] * coachingFoundationCut) /
             100000;
@@ -58,7 +61,10 @@ abstract contract CoachingManager is BasePlatform {
     }
 
     function createTeam(uint _coachingFee, bool _leaderDistributed) external {
-        /// @dev Modify to allow coaches to be a part of multiple teams
+        /// @notice Allows insturcters to initilize coaching teams. 
+        /// @param _coachingFee The fee for the coach.
+        /// @param _leaderDistributed explain
+        /// @dev to Burak: What is the unit of fee? Fraction? Please write info to param.
         require(udaoc.balanceOf(msg.sender) > 0, "You are not an instructor");
         require(ikyc.getKYC(msg.sender), "You are not KYCed");
         teamIds.increment();
@@ -71,6 +77,7 @@ abstract contract CoachingManager is BasePlatform {
     function setLeaderDistributed(uint _teamId, bool _leaderDistributed)
         external
     {
+        /// @dev explain
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -83,7 +90,11 @@ abstract contract CoachingManager is BasePlatform {
         address _coachAddress,
         uint _coachingFee
     ) external {
-        /// @dev Modify to allow coaches to be a part of multiple teams, move to off-chain maybe?
+        /// @notice Allows team leaders to add new coaches to their teams.
+        /// @dev Maybe we should also create another function add multiple coaches.
+        /// @param _teamId The ID of the team to add new coaches.
+        /// @param _coachAddress The address of the coach to add to the team
+        /// @param _coachingFee The fee for the coach
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -93,7 +104,10 @@ abstract contract CoachingManager is BasePlatform {
     }
 
     function removeCoachFromTeam(uint _teamId, address _coachAddress) external {
-        /// @dev Modify to allow coaches to be a part of multiple teams, move to off-chain maybe?
+        /// @notice Allows team leaders to remove coaches from their teams.
+        /// @dev Maybe we should also create another function add multiple coaches.
+        /// @param _teamId The ID of the team to remove a coach.
+        /// @param _coachAddress The address of the coach to remove from the team
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -107,6 +121,10 @@ abstract contract CoachingManager is BasePlatform {
         address _coachAddress,
         uint _coachingFee
     ) external {
+        /// @notice Allows team leaders to modify coaching fees in their teams.
+        /// @param _teamId The ID of the team to make changes.
+        /// @param _coachAddress The address of the coach to make changes.
+        /// @param _coachingFee The new coaching fee.
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -119,6 +137,7 @@ abstract contract CoachingManager is BasePlatform {
     }
 
     function withdrawTeam(uint _teamId) external {
+        /// @dev explain
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -127,6 +146,7 @@ abstract contract CoachingManager is BasePlatform {
     }
 
     function withdrawCoach() external {
+        /// @dev explain
         udao.transfer(msg.sender, coachBalance[msg.sender]);
     }
 }
