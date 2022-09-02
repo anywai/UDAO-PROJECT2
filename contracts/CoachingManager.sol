b8bb7926d37063aad7e3b0567e426c17a938268c
bugra <bugrahan.duran@anywai.ch>
2022-09-02 11:02:08 +0300

    feat: Multiple removal and additions of coaches

--- contracts/CoachingManager.sol
+++ contracts/CoachingManager.sol
@@ -62,9 +62,8 @@ abstract contract CoachingManager is BasePlatform {
 
     function createTeam(uint _coachingFee, bool _leaderDistributed) external {
         /// @notice Allows insturcters to initilize coaching teams. 
-        /// @param _coachingFee The fee for the coach.
-        /// @param _leaderDistributed explain
-        /// @dev to Burak: What is the unit of fee? Fraction? Please write info to param.
+        /// @param _coachingFee The fee for the coach in unit of UDAO.
+        /// @param _leaderDistributed Bool - If leader distributes off chain or on chain
         require(udaoc.balanceOf(msg.sender) > 0, "You are not an instructor");
         require(ikyc.getKYC(msg.sender), "You are not KYCed");
         teamIds.increment();
@@ -77,12 +76,14 @@ abstract contract CoachingManager is BasePlatform {
     function setLeaderDistributed(uint _teamId, bool _leaderDistributed)
         external
     {
-        /// @dev explain
+        /// @notice Set if the money distributed by the leader or by smart contract.
+        /// @param _teamId The ID of the team.
+        /// @param _leaderDistributed Bool - If leader distributes off chain or on chain
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
         );
-        leaderDistributed[teamIds.current()] = _leaderDistributed;
+        leaderDistributed[_teamId] = _leaderDistributed;
     }
 
     function addCoachToTeam(
@@ -103,6 +104,27 @@ abstract contract CoachingManager is BasePlatform {
         coachingFees[_teamId][_coachAddress] = _coachingFee;
     }
 
+    function addCoachesToTeam(
+        uint _teamId,
+        address[] calldata _coachAddresses,
+        uint[] calldata _coachingFees
+    ) external {
+        /// @notice Allows team leaders to add multiple new coaches to their teams.
+        /// @param _teamId The ID of the team to add new coaches.
+        /// @param _coachAddresses The addresses of the coaches to add to the team
+        /// @param _coachingFees The fees for the coaches
+        require(
+            teamLeader[_teamId] == msg.sender,
+            "You are not the team leader."
+        );
+        require(_coachAddresses.length == _coachingFees.length, "There should be same amount of coaches and fees.");
+        for (uint i; i<_coachAddresses.length; i++){
+            teamMembers[_teamId][_coachAddresses[i]] = true;
+            coachingFees[_teamId][_coachAddresses[i]] = _coachingFees[i];
+        }
+
+    }
+
     function removeCoachFromTeam(uint _teamId, address _coachAddress) external {
         /// @notice Allows team leaders to remove coaches from their teams.
         /// @dev Maybe we should also create another function add multiple coaches.
@@ -114,6 +136,22 @@ abstract contract CoachingManager is BasePlatform {
         );
         teamMembers[_teamId][_coachAddress] = false;
         coachingFees[_teamId][_coachAddress] = 0;
+    }
+
+    function removeCoachesFromTeam(uint _teamId, address[] calldata _coachAddresses) external {
+        /// @notice Allows team leaders to remove multiple coaches from their teams.
+        /// @dev Maybe we should also create another function add multiple coaches.
+        /// @param _teamId The ID of the team to remove a coach.
+        /// @param _coachAddresses The addresses of the coaches to remove from the team
+        require(
+            teamLeader[_teamId] == msg.sender,
+            "You are not the team leader."
+        );
+        for (uint i;i<_coachAddresses.length;i++){
+            teamMembers[_teamId][_coachAddresses[i]] = false;
+            coachingFees[_teamId][_coachAddresses[i]] = 0;
+        }
+
     }
 
     function setCoachingFee(
@@ -136,8 +174,33 @@ abstract contract CoachingManager is BasePlatform {
         coachingFees[_teamId][_coachAddress] = _coachingFee;
     }
 
+    function setCoachingFees(
+        uint _teamId,
+        address[] calldata _coachAddresses,
+        uint[] calldata _coachingFees
+    ) external {
+        /// @notice Allows team leaders to modify coaching fees in their teams.
+        /// @param _teamId The ID of the team to make changes.
+        /// @param _coachAddresses The addresses of the coaches to make changes.
+        /// @param _coachingFees The new coaching fees.
+        require(
+            teamLeader[_teamId] == msg.sender,
+            "You are not the team leader."
+        );
+        require(_coachAddresses.length == _coachingFees.length, "There should be same amount of coaches and fees.");
+        for(uint i; i<_coachAddresses.length;i++){
+            require(
+                teamMembers[_teamId][_coachAddresses[i]] == true,
+                "This coach doesn't belong to this team."
+            );
+            coachingFees[_teamId][_coachAddresses[i]] = _coachingFees[i];
+        }
+
+    }
+
     function withdrawTeam(uint _teamId) external {
-        /// @dev explain
+        /// @notice Allows team leader to withdraw.
+        /// @param _teamId The ID of the team.
         require(
             teamLeader[_teamId] == msg.sender,
             "You are not the team leader."
@@ -146,7 +209,7 @@ abstract contract CoachingManager is BasePlatform {
     }
 
     function withdrawCoach() external {
-        /// @dev explain
+        /// @dev Allows coaches to withdraw individually.
         udao.transfer(msg.sender, coachBalance[msg.sender]);
     }
 }
