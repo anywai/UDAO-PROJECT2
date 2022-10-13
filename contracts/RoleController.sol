01fb9195edaf922257179ec610c69efb7f62a6d7
Burak Özen <burak.ozen@anywai.ch>
2022-10-13 18:35:54 +0300

    feat: add role controller for contracts to access

---
+++ contracts/RoleController.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+/// @title #########################
+pragma solidity ^0.8.4;
+
+import "./IRoleManager.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+abstract contract RoleController {
+    bytes32 public constant VALIDATOR_ROLE = keccak256("VALIDATOR_ROLE");
+    bytes32 public constant SUPER_VALIDATOR_ROLE =
+        keccak256("SUPER_VALIDATOR_ROLE");
+    bytes32 public constant BACKEND_ROLE = keccak256("BACKEND_ROLE");
+    bytes32 public constant FOUNDATION_ROLE = keccak256("FOUNDATION_ROLE");
+    bytes32 public constant STAKING_CONTRACT = keccak256("STAKING_CONTRACT");
+    bytes32 public constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
+    bytes32 public constant JUROR_ROLE = keccak256("JUROR_ROLE");
+    bytes32 public constant JUROR_CONTRACT = keccak256("JUROR_CONTRACT");
+
+    IRoleManager irm;
+
+    modifier onlyRole(bytes32 role) {
+        irm.checkRole(role, _msgSender());
+        _;
+    }
+    modifier onlyRoles(bytes32 roles) {
+        irm.checkRoles(roles, _msgSender());
+        _;
+    }
+
+    constructor(address irmAddress) {
+        irm = IRoleManager(irmAddress);
+    }
+}
