06653429d25adbc14580ca6e665493f13d8fe3c2
bugradrn <bugrahan.duran@anywai.ch>
2022-10-17 13:16:37 +0300

    feat: Lazy minting of UDAOC

--- contracts/UDAOC.sol
+++ contracts/UDAOC.sol
@@ -5,13 +5,18 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "./IKYC.sol";
 import "./RoleManager.sol";
 
-contract UDAOContent is ERC721, ERC721URIStorage, RoleManager {
+contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleManager{
     using Counters for Counters.Counter;
 
     IKYC ikyc;
+
+    string private constant SIGNING_DOMAIN = "UDAOCMinter";
+    string private constant SIGNATURE_VERSION = "1";
 
     Counters.Counter private _tokenIdCounter;
 
@@ -20,56 +25,87 @@ contract UDAOContent is ERC721, ERC721URIStorage, RoleManager {
     // tokenId => price
     mapping(uint => uint) contentPrice;
 
-    constructor(address _kycAddress) ERC721("UDAO Content", "UDAOC") {
+    constructor(address _kycAddress) 
+    ERC721("UDAO Content", "UDAOC")
+    EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
+    {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         ikyc = IKYC(_kycAddress);
     }
 
-    /// @dev add KYC control
-    /// change to lazy mint
-    function safeMint(
-        address to,
-        string memory uri,
-        uint _contentPrice
-    ) public {
-        /// @notice mints content token to `to` address
-        /// @param to address of content owner
-        /// @param uri URI address of content
-        /// @param _contentPrice price of content
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        contentPrice[tokenId] = _contentPrice;
+    /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain.
+    /// A signed voucher can be redeemed for a real NFT using the redeem function.
+    struct NFTVoucher {
+        /// @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
+        uint256 tokenId;
+        /// @notice The price of the content
+        uint256 contentPrice;
+        /// @notice The metadata URI to associate with this token.
+        string uri;
+        /// @notice The name of the NFT
+        string name;
+        /// @notice The descriptiom of the NFT
+        string description;
+        /// @notice the EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by an account with the MINTER_ROLE.
+        bytes signature;
     }
 
-    // The following functions are overrides required by Solidity.
+    /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
+    /// @param redeemer The address of the account which will receive the NFT upon success.
+    /// @param to the id of the donee which is used to get the address of it.
+    /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
+    function redeem(address redeemer, address to, NFTVoucher calldata voucher) public payable returns (uint256) {
+        // make sure signature is valid and get the address of the signer
+        address signer = _verify(voucher);
 
-    /// @dev her token'ın kendi URI'yı olacağı için override kısmına taşıyıp comment out ettim
-    // function _baseURI() internal view override returns (string memory) {
-    //     return defaultURI;
-    // }
+        // first assign the token to the signer, to establish provenance on-chain
+        _mint(signer, voucher.tokenId);
+        _setTokenURI(voucher.tokenId, voucher.uri);
 
-    // function setBaseURI(string calldata _newURI)
-    //     external
-    //     onlyRole(DEFAULT_ADMIN_ROLE)
-    // {
-    //     defaultURI = _newURI;
-    // }
+        // save the content price
+        contentPrice[voucher.tokenId] = voucher.contentPrice;
+
+        // transfer the token to the redeemer
+        _transfer(signer, redeemer, voucher.tokenId);
+    }
+
+    /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
+    /// @param voucher An NFTVoucher to hash.
+    function _hash(NFTVoucher calldata voucher) internal view returns (bytes32) {
+    return _hashTypedDataV4(keccak256(abi.encode(
+        keccak256("UDAOCMinter(uint256 tokenId,uint256 contentPrice,string uri,string name,string description)"),
+        voucher.tokenId,
+        voucher.contentPrice,
+        keccak256(bytes(voucher.uri)),
+        keccak256(bytes(voucher.name)),
+        keccak256(bytes(voucher.description))
+        )));
+    }
+
+    /// @notice Returns the chain id of the current blockchain.
+    /// @dev This is used to workaround an issue with ganache returning different values from the on-chain chainid() function and
+    ///  the eth_chainId RPC method. See https://github.com/protocol/nft-website/issues/121 for context.
+    function getChainID() external view returns (uint256) {
+        uint256 id;
+        assembly {
+        id := chainid()
+        }
+        return id;
+    }
+
+    /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
+    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+    /// @param voucher An NFTVoucher describing an unminted NFT.
+    function _verify(NFTVoucher calldata voucher) internal view returns (address) {
+        bytes32 digest = _hash(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+
     function _burn(uint256 tokenId)
         internal
         override(ERC721, ERC721URIStorage)
     {
         super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     // Getters
@@ -91,13 +127,21 @@ contract UDAOContent is ERC721, ERC721URIStorage, RoleManager {
 
     function setKycContractAddress(address _kycAddress)
         external
-        onlyRole(FOUNDATION_ROLE)
     {
         ikyc = IKYC(address(_kycAddress));
     }
 
     // The following functions are overrides required by Solidity.
 
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    
     function supportsInterface(bytes4 interfaceId)
         public
         view
