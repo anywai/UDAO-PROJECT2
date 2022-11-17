4c6e89950de69e0e7b6d09e56518d5d30233780b
bugradrn <bugrahan.duran@anywai.ch>
2022-11-17 13:05:18 +0300

    test: Added new tests

---
+++ test/ValidationManager-tests.js
@@ -0,0 +1,352 @@
+const { expect } = require("chai");
+const hardhat = require("hardhat");
+const { ethers } = hardhat;
+const chai = require("chai");
+const BN = require("bn.js");
+const { LazyMinter } = require("../lib/LazyMinter");
+const { LazyRole } = require("../lib/LazyRole");
+const { LazyScore } = require("../lib/LazyScore");
+const { LazyValidation } = require("../lib/LazyValidation");
+const { LazyUDAOCertMinter } = require("../lib/LazyUDAOCertMinter");
+
+// Enable and inject BN dependency
+chai.use(require("chai-bn")(BN));
+
+async function deploy() {
+    const [
+        backend,
+        contentCreator,
+        contentBuyer,
+        validatorCandidate,
+        validator,
+        superValidatorCandidate,
+        superValidator,
+        foundation,
+        governanceCandidate,
+        governanceMember,
+        jurorCandidate,
+        jurorMember,
+    ] = await ethers.getSigners();
+
+    // FACTORIES
+    let factoryRoleManager = await ethers.getContractFactory("RoleManager");
+    let factoryUDAOVp = await ethers.getContractFactory("UDAOVp");
+    let factoryUDAOTimelockController = await ethers.getContractFactory(
+        "UDAOTimelockController"
+    );
+    let factoryUDAOCertificate = await ethers.getContractFactory(
+        "UDAOCertificate"
+    );
+    let factoryUDAO = await ethers.getContractFactory("UDAO");
+    let factoryUDAOStaker = await ethers.getContractFactory("UDAOStaker");
+    let factoryValidationManager = await ethers.getContractFactory(
+        "ValidationManager"
+    );
+    let factoryUDAOContent = await ethers.getContractFactory("UDAOContent");
+    let factoryPlatformTreasury = await ethers.getContractFactory(
+        "PlatformTreasury"
+    );
+    let factoryUDAOGovernor = await ethers.getContractFactory("UDAOGovernor");
+
+    //DEPLOYMENTS
+    const contractUDAO = await factoryUDAO.deploy();
+    const contractRoleManager = await factoryRoleManager.deploy();
+    const contractUDAOCertificate = await factoryUDAOCertificate.deploy(
+        contractRoleManager.address
+    );
+    const contractUDAOContent = await factoryUDAOContent.deploy(
+        contractRoleManager.address
+    );
+    const contractValidationManager = await factoryValidationManager.deploy(
+        contractUDAOContent.address,
+        contractRoleManager.address
+    );
+    const contractPlatformTreasury = await factoryPlatformTreasury.deploy(
+        contractUDAO.address,
+        contractUDAOContent.address,
+        contractRoleManager.address,
+        contractValidationManager.address
+    );
+    const contractUDAOVp = await factoryUDAOVp.deploy(
+        contractRoleManager.address
+    );
+    const contractUDAOStaker = await factoryUDAOStaker.deploy(
+        contractUDAOVp.address,
+        contractUDAO.address,
+        contractPlatformTreasury.address,
+        contractRoleManager.address
+    );
+    const contractUDAOTimelockController =
+        await factoryUDAOTimelockController.deploy(1, [], [foundation.address]);
+    const contractUDAOGovernor = await factoryUDAOGovernor.deploy(
+        contractUDAOVp.address,
+        contractUDAOTimelockController.address,
+        contractUDAOStaker.address,
+        contractRoleManager.address
+    );
+    //POST DEPLOYMENT
+    // add proposer
+    const PROPOSER_ROLE = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("PROPOSER_ROLE")
+    );
+    await contractUDAOTimelockController.grantRole(
+        PROPOSER_ROLE,
+        contractUDAOGovernor.address
+    );
+
+    // grant roles
+    const BACKEND_ROLE = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("BACKEND_ROLE")
+    );
+    await contractRoleManager.grantRole(BACKEND_ROLE, backend.address);
+    const FOUNDATION_ROLE = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("FOUNDATION_ROLE")
+    );
+
+    await contractRoleManager.grantRole(FOUNDATION_ROLE, foundation.address);
+    const STAKING_CONTRACT = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("STAKING_CONTRACT")
+    );
+    await contractRoleManager.grantRole(
+        STAKING_CONTRACT,
+        contractUDAOStaker.address
+    );
+    const GOVERNANCE_ROLE = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("GOVERNANCE_ROLE")
+    );
+    await contractRoleManager.grantRole(
+        GOVERNANCE_ROLE,
+        contractUDAOTimelockController.address
+    );
+    const VALIDATION_MANAGER = ethers.utils.keccak256(
+        ethers.utils.toUtf8Bytes("VALIDATION_MANAGER")
+    );
+    await contractRoleManager.grantRole(
+        VALIDATION_MANAGER,
+        contractValidationManager.address
+    );
+
+    // add staking contract to validation manager
+    await contractValidationManager
+        .connect(foundation)
+        .setStaker(contractUDAOStaker.address);
+    // add staking contract to udao-vp
+    await contractUDAOVp
+        .connect(foundation)
+        .setStakingContract(contractUDAOStaker.address);
+
+    return {
+        backend,
+        contentCreator,
+        contentBuyer,
+        validatorCandidate,
+        validator,
+        superValidatorCandidate,
+        superValidator,
+        foundation,
+        governanceCandidate,
+        governanceMember,
+        jurorCandidate,
+        jurorMember,
+        contractUDAO,
+        contractRoleManager,
+        contractUDAOCertificate,
+        contractUDAOContent,
+        contractValidationManager,
+        contractPlatformTreasury,
+        contractUDAOVp,
+        contractUDAOStaker,
+        contractUDAOTimelockController,
+        contractUDAOGovernor,
+    };
+}
+describe("UDAO Project", function () {
+    it("Should deploy", async function () {
+        const {
+            backend,
+            validatorCandidate,
+            validator,
+            superValidatorCandidate,
+            superValidator,
+            foundation,
+            governanceCandidate,
+            governanceMember,
+            jurorCandidate,
+            jurorMember,
+            contractUDAO,
+            contractRoleManager,
+            contractUDAOCertificate,
+            contractUDAOContent,
+            contractValidationManager,
+            contractPlatformTreasury,
+            contractUDAOVp,
+            contractUDAOStaker,
+            contractUDAOTimelockController,
+            contractUDAOGovernor,
+        } = await deploy();
+    });
+
+    it("Should validate content", async function () {
+        const {
+            backend,
+            contentCreator,
+            contentBuyer,
+            validatorCandidate,
+            validator,
+            superValidatorCandidate,
+            superValidator,
+            foundation,
+            governanceCandidate,
+            governanceMember,
+            jurorCandidate,
+            jurorMember,
+            contractUDAO,
+            contractRoleManager,
+            contractUDAOCertificate,
+            contractUDAOContent,
+            contractValidationManager,
+            contractPlatformTreasury,
+            contractUDAOVp,
+            contractUDAOStaker,
+            contractUDAOTimelockController,
+            contractUDAOGovernor,
+        } = await deploy();
+        await contractRoleManager.setKYC(contentCreator.address, true);
+
+        const tx_mint = await contractUDAOContent.getChainID();
+        const lazyMinter = new LazyMinter({
+            contract: contractUDAOContent,
+            signer: backend,
+        });
+        const voucher_udaoc = await lazyMinter.createVoucher(
+            1,
+            [ethers.utils.parseEther("1.0"), ethers.utils.parseEther("2.0")],
+            "ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi",
+            contentCreator.address,
+            "Content Name",
+            "Content Description"
+        );
+        await expect(contractUDAOContent.connect(contentCreator).redeem(voucher_udaoc))
+            .to.emit(contractUDAOContent, "Transfer") // transfer from null address to minter
+            .withArgs(
+                "0x0000000000000000000000000000000000000000",
+                contentCreator.address,
+                voucher_udaoc.tokenId
+            );
+
+        const tx = await contractValidationManager.getChainID();
+        const lazyValidation = new LazyValidation({
+            contract: contractValidationManager,
+            signer: backend,
+        });
+        const voucher = await lazyValidation.createVoucher(
+            1,
+            contentCreator.address
+        );
+        await expect(contractValidationManager.connect(contentCreator).setAsValidated(voucher))
+            .to.emit(contractValidationManager, "ValidationEnded")
+            .withArgs(
+                voucher.tokenId,
+                true
+            );
+    });
+
+    it("Should not validate content if not exist", async function () {
+        const {
+            backend,
+            contentCreator,
+            contentBuyer,
+            validatorCandidate,
+            validator,
+            superValidatorCandidate,
+            superValidator,
+            foundation,
+            governanceCandidate,
+            governanceMember,
+            jurorCandidate,
+            jurorMember,
+            contractUDAO,
+            contractRoleManager,
+            contractUDAOCertificate,
+            contractUDAOContent,
+            contractValidationManager,
+            contractPlatformTreasury,
+            contractUDAOVp,
+            contractUDAOStaker,
+            contractUDAOTimelockController,
+            contractUDAOGovernor,
+        } = await deploy();
+        await contractRoleManager.setKYC(contentCreator.address, true);
+
+        const tx = await contractValidationManager.getChainID();
+        const lazyValidation = new LazyValidation({
+            contract: contractValidationManager,
+            signer: backend,
+        });
+        const voucher = await lazyValidation.createVoucher(
+            1,
+            contentCreator.address
+        );
+        await expect(contractValidationManager.connect(contentCreator).setAsValidated(voucher)).to.revertedWith('ERC721: invalid token ID')
+    });
+
+    it("Should not validate content if redeemer is not owner of token", async function () {
+      const {
+          backend,
+          contentCreator,
+          contentBuyer,
+          validatorCandidate,
+          validator,
+          superValidatorCandidate,
+          superValidator,
+          foundation,
+          governanceCandidate,
+          governanceMember,
+          jurorCandidate,
+          jurorMember,
+          contractUDAO,
+          contractRoleManager,
+          contractUDAOCertificate,
+          contractUDAOContent,
+          contractValidationManager,
+          contractPlatformTreasury,
+          contractUDAOVp,
+          contractUDAOStaker,
+          contractUDAOTimelockController,
+          contractUDAOGovernor,
+      } = await deploy();
+      await contractRoleManager.setKYC(contentCreator.address, true);
+
+      const tx_mint = await contractUDAOContent.getChainID();
+      const lazyMinter = new LazyMinter({
+          contract: contractUDAOContent,
+          signer: backend,
+      });
+      const voucher_udaoc = await lazyMinter.createVoucher(
+          1,
+          [ethers.utils.parseEther("1.0"), ethers.utils.parseEther("2.0")],
+          "ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi",
+          contentCreator.address,
+          "Content Name",
+          "Content Description"
+      );
+      await expect(contractUDAOContent.connect(contentCreator).redeem(voucher_udaoc))
+          .to.emit(contractUDAOContent, "Transfer") // transfer from null address to minter
+          .withArgs(
+              "0x0000000000000000000000000000000000000000",
+              contentCreator.address,
+              voucher_udaoc.tokenId
+          );
+
+      const tx = await contractValidationManager.getChainID();
+      const lazyValidation = new LazyValidation({
+          contract: contractValidationManager,
+          signer: backend,
+      });
+      const voucher = await lazyValidation.createVoucher(
+          1,
+          contentBuyer.address
+      );
+      await expect(contractValidationManager.connect(contentBuyer).setAsValidated(voucher)).to.revertedWith("Redeemer is not the owner of the token");
+  });
+});
