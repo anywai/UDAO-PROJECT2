20c22d34fc0de840334ad2d2324c75a7f0b92e14
Burak Özen <burak.ozen@anywai.ch>
2022-11-23 11:14:27 +0300

    chore: add and configure gas cost reporter
    Co-authored-by: Bugrahan Duran <bugrahan.duran@anywai.ch>

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("solidity-docgen");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
 require("solidity-coverage");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -24,12 +25,19 @@ module.exports = {
   networks: {
     hardhat: { chainId: 31337 },
   },
+  gasReporter: {
+    currency: "EUR",
+    coinmarketcap: "9b067bab-555e-4a2e-8b46-3ddbf5254166",
+    gasPriceApi:
+      "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    outputFile: "./output.txt",
+  },
   solidity: {
-    version: "0.8.4",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 50,
+        runs: 200,
       },
     },
   },
