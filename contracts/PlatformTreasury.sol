48cf77177cc34d5c2379fe1dd41cfe17065a0452
bugradrn <bugrahan.duran@anywai.ch>
2022-12-07 12:57:20 +0300

    refactor: Removed ScoreVoucher
     Co-authored-by: Burak Özen <burak.ozen@anywai.ch>

--- contracts/PlatformTreasury.sol
+++ contracts/PlatformTreasury.sol
@@ -24,20 +24,6 @@ contract PlatformTreasury is Pausable, ContentManager {
         ContentManager(vmAddress)
     {}
 
-    /// @notice The id of the token to be redeemed.
-    struct ScoreVoucher {
-        /// @dev Current accumulated score points in off-chain
-        int256 score;
-        /// @dev The number of validation that matched with the final verdict
-        uint256 successfulValidation;
-        /// @dev The number of validation that didn't match with the final verdict
-        uint256 unsuccessfulValidation;
-        /// @dev Address of the redeemer (validator)
-        address redeemer;
-        /// @dev the EIP-712 signature of all other fields in the ContentVoucher struct
-        bytes signature;
-    }
-
     // validator => score
     mapping(address => int256) validatorScore;
 
@@ -51,33 +37,10 @@ contract PlatformTreasury is Pausable, ContentManager {
         udao.transfer(foundationWallet, foundationBalance);
     }
 
-    /// @notice Allows validators to record their score on-chain
-    /// @param _scoreVoucher The voucher of the score information
-    function writeValidatorScore(ScoreVoucher calldata _scoreVoucher)
-        external
-        onlyRoles(validator_roles)
-    {
-        // make sure redeemer is redeeming
-        require(
-            _scoreVoucher.redeemer == msg.sender,
-            "You are not the redeemer"
-        );
-
-        // make sure signature is valid and get the address of the signer
-        address signer = _verify(_scoreVoucher);
-        require(
-            IRM.hasRole(BACKEND_ROLE, signer),
-            "Signature invalid or unauthorized"
-        );
-
-        validatorScore[_scoreVoucher.redeemer] = _scoreVoucher.score;
-    }
-
     /// @notice calculates validator earnings and withdraws calculated earning to validator wallet
     function withdrawValidator() external onlyRoles(validator_roles) {
-        uint[2] memory results = udaoc.getValidationResults(msg.sender);
         /// @dev results[0] is successful validations
-        uint participation = (results[0] * 100000) / udaoc.getTotalValidation();
+        uint participation = IVM.getValidatorScore(msg.sender) / IVM.getTotalValidation();
         uint earning = (participation * validatorBalance) / 100000;
         udao.transfer(msg.sender, earning);
     }
@@ -87,38 +50,4 @@ contract PlatformTreasury is Pausable, ContentManager {
         udao.transfer(msg.sender, instructorBalance[msg.sender]);
     }
 
-    /// @notice Returns a hash of the given ScoreVoucher, prepared using EIP712 typed data hashing rules.
-    /// @param voucher A ScoreVoucher to hash.
-    function _hash(ScoreVoucher calldata voucher)
-        internal
-        view
-        returns (bytes32)
-    {
-        return
-            _hashTypedDataV4(
-                keccak256(
-                    abi.encode(
-                        keccak256(
-                            "ScoreVoucher(int256 score,uint256 successfulValidation,uint256 unsuccessfulValidation,address redeemer)"
-                        ),
-                        voucher.score,
-                        voucher.successfulValidation,
-                        voucher.unsuccessfulValidation,
-                        voucher.redeemer
-                    )
-                )
-            );
-    }
-
-    /// @notice Verifies the signature for a given ScoreVoucher, returning the address of the signer.
-    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
-    /// @param voucher A ScoreVoucher describing an unminted NFT.
-    function _verify(ScoreVoucher calldata voucher)
-        internal
-        view
-        returns (address)
-    {
-        bytes32 digest = _hash(voucher);
-        return ECDSA.recover(digest, voucher.signature);
-    }
 }
