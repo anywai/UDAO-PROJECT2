ebbaa5cb68ec5c96be1978e1613aa04ac7ebeb27
bugradrn <bugrahan.duran@anywai.ch>
2023-01-18 12:55:43 +0300

    feat: Content token transfer with and without voucher enabled
    Co-authored-by: Burak Özen <burak.ozen@anywai.ch>

--- contracts/UDAOC.sol
+++ contracts/UDAOC.sol
@@ -13,6 +13,7 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
     string private constant SIGNING_DOMAIN = "UDAOCMinter";
     string private constant SIGNATURE_VERSION = "1";
 
+    bool isKycChecked;
     /// @param rmAddress The address of the deployed role manager
     constructor(
         address rmAddress
@@ -20,7 +21,7 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
         ERC721("UDAO Content", "UDAOC")
         EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
         RoleController(rmAddress)
-    {}
+    {isKycChecked=true;}
 
     /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain.
     /// A signed voucher can be redeemed for a real NFT using the redeem function.
@@ -44,6 +45,38 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
     // tokenId => is coaching service buyable
     mapping(uint => bool) coachingEnabled;
 
+    /// @notice For transfers with "transfer" function
+    struct TransferVoucher {
+        /// @notice The id of the token to be redeemed.
+        uint256 tokenId;
+        /// @notice Address of the current owner
+        address from;
+        /// @notice Address of the redeemer
+        address to;
+        /// @notice the EIP-712 signature of all other fields in the ContentVoucher struct.
+        bytes signature;
+    }
+
+    /// @notice For transfers with "safeTransferFrom" functions
+    struct TransferVoucherData {
+        /// @notice The id of the token to be redeemed.
+        uint256 tokenId;
+        /// @notice Address of the current owner
+        address from;
+        /// @notice Address of the redeemer
+        address to;
+        /// @notice Data variable in "safeTransferFrom"
+        bytes data;
+        /// @notice the EIP-712 signature of all other fields in the ContentVoucher struct.
+        bytes signature;
+    }
+
+    uint256 private constant _NOT_VOUCHER = 1;
+    uint256 private constant _VOUCHER = 2;
+
+    uint256 private _is_voucher;
+
+
     /// @notice Redeems a ContentVoucher for an actual NFT, creating it in the process.
     /// @param voucher A signed ContentVoucher that describes the NFT to be redeemed.
     function redeem(ContentVoucher calldata voucher) public {
@@ -51,7 +84,7 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
         require(voucher.redeemer == msg.sender, "You are not the redeemer");
 
         // make sure signature is valid and get the address of the signer
-        address signer = _verify(voucher);
+        address signer = _verifyRedeem(voucher);
         require(
             IRM.hasRole(BACKEND_ROLE, signer),
             "Signature invalid or unauthorized"
@@ -81,13 +114,56 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
         coachingEnabled[tokenId] = false;
     }
 
+    /// @notice Returns if a coaching enabled for a token or not
     function isCoachingEnabled(uint tokenId) external view returns (bool) {
         return coachingEnabled[tokenId];
     }
 
+
+    /// @notice Allows backend to either to force or not the KYC check before transfers 
+    function setIsKycChecked(bool _isKycChecked) external onlyRole(FOUNDATION_ROLE){
+        isKycChecked = _isKycChecked;
+    }
+
+    /// @notice ERC721 transferFrom with a voucher
+    function voucherTransferFrom(TransferVoucher calldata voucher) public {
+        address signer = _verifyTransfer(voucher);
+        require(
+            IRM.hasRole(BACKEND_ROLE, signer),
+            "Signature invalid or unauthorized"
+        );
+        _is_voucher = _VOUCHER;
+        transferFrom(voucher.from,voucher.to, voucher.tokenId);
+        _is_voucher = _NOT_VOUCHER;
+    }
+
+    /// @notice ERC721 safeTransferFrom with a voucher
+    function voucherSafeTransferFrom(TransferVoucher calldata voucher) public {
+        address signer = _verifyTransfer(voucher);
+        require(
+            IRM.hasRole(BACKEND_ROLE, signer),
+            "Signature invalid or unauthorized"
+        );
+        _is_voucher = _VOUCHER;
+        safeTransferFrom(voucher.from,voucher.to, voucher.tokenId);
+        _is_voucher = _NOT_VOUCHER;
+    }
+
+    /// @notice ERC721 safeTransferFrom with data variable with a voucher
+    function voucherSafeTransferFrom(TransferVoucherData calldata voucher) public {
+        address signer = _verifyTransferData(voucher);
+        require(
+            IRM.hasRole(BACKEND_ROLE, signer),
+            "Signature invalid or unauthorized"
+        );
+        _is_voucher = _VOUCHER;
+        safeTransferFrom(voucher.from,voucher.to, voucher.tokenId, voucher.data);
+        _is_voucher = _NOT_VOUCHER;
+    }
+
     /// @notice Returns a hash of the given ContentVoucher, prepared using EIP712 typed data hashing rules.
     /// @param voucher A ContentVoucher to hash.
-    function _hash(
+    function _hashRedeem(
         ContentVoucher calldata voucher
     ) internal view returns (bytes32) {
         return
@@ -108,6 +184,47 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
             );
     }
 
+    /// @notice Returns a hash of the given TransferVoucher, prepared using EIP712 typed data hashing rules.
+    /// @param voucher A TransferVoucher to hash.
+    function _hashTransfer(
+        TransferVoucher calldata voucher
+    ) internal view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "TransferVoucher(uint256 tokenId,address from,address to)"
+                        ),
+                        voucher.tokenId,
+                        voucher.from,
+                        voucher.to
+                    )
+                )
+            );
+    }
+
+    /// @notice Returns a hash of the given TransferVoucherData, prepared using EIP712 typed data hashing rules.
+    /// @param voucher A TransferVoucherData to hash.
+    function _hashTransferData(
+        TransferVoucherData calldata voucher
+    ) internal view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "TransferVoucherData(uint256 tokenId,address from,address to,bytes data)"
+                        ),
+                        voucher.tokenId,
+                        voucher.from,
+                        voucher.to,
+                        voucher.data
+                    )
+                )
+            );
+    }
+
     /// @notice Returns the chain id of the current blockchain.
     /// @dev This is used to workaround an issue with ganache returning different values from the on-chain chainid() function and
     ///  the eth_chainId RPC method. See https://github.com/protocol/nft-website/issues/121 for context.
@@ -122,10 +239,30 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
     /// @notice Verifies the signature for a given ContentVoucher, returning the address of the signer.
     /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
     /// @param voucher A ContentVoucher describing an unminted NFT.
-    function _verify(
+    function _verifyRedeem(
         ContentVoucher calldata voucher
     ) internal view returns (address) {
-        bytes32 digest = _hash(voucher);
+        bytes32 digest = _hashRedeem(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+
+     /// @notice Verifies the signature for a given transferVoucher, returning the address of the signer.
+    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+    /// @param voucher A transferVoucher describing an unminted NFT.
+    function _verifyTransfer(
+        TransferVoucher calldata voucher
+    ) internal view returns (address) {
+        bytes32 digest = _hashTransfer(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+
+    /// @notice Verifies the signature for a given transferVoucherData, returning the address of the signer.
+    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+    /// @param voucher A transferVoucherData describing an unminted NFT.
+    function _verifyTransferData(
+        TransferVoucherData calldata voucher
+    ) internal view returns (address) {
+        bytes32 digest = _hashTransferData(voucher);
         return ECDSA.recover(digest, voucher.signature);
     }
 
@@ -144,24 +281,19 @@ contract UDAOContent is ERC721, EIP712, ERC721URIStorage, RoleController {
     ) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
-
+     
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId
     ) internal virtual override {
-        /// @notice make sure the transfer is made to a KYCed wallet
-        /// TODO Removing KYC from here could be a problem.
         super._beforeTokenTransfer(from, to, tokenId);
-        if (to != address(0)) {
-            require(IRM.isKYCed(to), "Receiver is not KYCed!");
-            require(!IRM.isBanned(to), "Receiver is banned!");
-        }
-        if (from != address(0)) {
-            require(IRM.isKYCed(from), "Sender is not KYCed!");
-            require(!IRM.isBanned(from), "Sender is banned!");
-        }
+        if(isKycChecked) { /// @notice if KYC check is active with vouchers
+            require(_is_voucher == _VOUCHER, "Non voucher transfers are not allowed");
+        } 
     }
+
+    
 
     /// @notice Allows off-chain check if a token(content) exists
     function exists(uint tokenId) external view returns (bool) {
