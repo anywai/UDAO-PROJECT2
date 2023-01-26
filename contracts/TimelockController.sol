f231302e21deca845e5d14bf11032425cae1a36e
bugradrn <bugrahan.duran@anywai.ch>
2023-01-26 19:11:57 +0300

    docs: Added comments for execute

--- contracts/TimelockController.sol
+++ contracts/TimelockController.sol
@@ -11,8 +11,16 @@ contract UDAOTimelockController is TimelockController {
         address[] memory executors
     ) TimelockController(minDelay, proposers, executors) {}
 
+    /*
     /// @notice executes the acceepted proposal
-    /// TODO Write Parameters
+    /// @param  target the address of the smart contract that the timelock should operate on
+    /// @param value  in wei, that should be sent with the transaction. Most of the time this will be 0
+    /// @param data containing the encoded function selector and parameters of the call. 
+    This can be produced using a number of tools.
+    /// @param predecessor that specifies a dependency between operations. 
+    This dependency is optional. Use bytes32(0) if the operation does not have any dependency.
+    /// @param salt  Used to disambiguate two otherwise identical operations. This can be any random value.
+    */
     function execute(
         address target,
         uint256 value,
