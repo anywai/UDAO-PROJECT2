799f4349c912d1bd81516c7856cf4d3a575bd62d
mbd <bugrahan.duran@anywai.ch>
2023-05-09 10:43:14 +0300

    refactor(timelock): Removed unused function

--- contracts/TimelockController.sol
+++ contracts/TimelockController.sol
@@ -10,30 +10,4 @@ contract UDAOTimelockController is TimelockController {
         address[] memory proposers,
         address[] memory executors
     ) TimelockController(minDelay, proposers, executors) {}
-
-    /*
-    /// @notice executes the acceepted proposal
-    /// @param  target the address of the smart contract that the timelock should operate on
-    /// @param value  in wei, that should be sent with the transaction. Most of the time this will be 0
-    /// @param data containing the encoded function selector and parameters of the call. 
-    This can be produced using a number of tools.
-    /// @param predecessor that specifies a dependency between operations. 
-    This dependency is optional. Use bytes32(0) if the operation does not have any dependency.
-    /// @param salt  Used to disambiguate two otherwise identical operations. This can be any random value.
-    */
-    function execute(
-        address target,
-        uint256 value,
-        bytes calldata data,
-        bytes32 predecessor,
-        bytes32 salt
-    )
-        public
-        payable
-        virtual
-        override(TimelockController)
-        onlyRoleOrOpenRole(EXECUTOR_ROLE)
-    {
-        return super.execute(target, value, data, predecessor, salt);
-    }
 }
