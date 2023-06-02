1708f0cd69148aa3ed092a56deabaf3d31d874ed
Burak Özen <burak.ozen@anywai.ch>
2023-06-02 10:59:10 +0300

    chore: merge deployments of tests

--- test/PlatformTreasury-tests.js
+++ test/PlatformTreasury-tests.js
@@ -4,6 +4,7 @@ const { ethers } = hardhat;
 const chai = require("chai");
 const BN = require("bn.js");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const { deploy } = require("../lib/deployments");
 
 const {
   WMATIC_ABI,
@@ -299,376 +300,8 @@ async function makeCoachingPurchase(
   expect(coachingStruct.learner).to.equal(contentBuyer.address);
   return coachingId;
 }
-async function deploy(isDexRequired = false) {
-  if (isDexRequired) {
-    helpers.reset(
-      "https://polygon-mainnet.g.alchemy.com/v2/OsNaN43nxvV85Kk1JpU-a5qduFwjcIGJ",
-      40691400
-    );
-  }
-  const [
-    backend,
-    contentCreator,
-    contentBuyer1,
-    contentBuyer2,
-    contentBuyer3,
-    validatorCandidate,
-    validator1,
-    validator2,
-    validator3,
-    validator4,
-    validator5,
-    superValidatorCandidate,
-    superValidator,
-    foundation,
-    governanceCandidate,
-    governanceMember,
-    jurorCandidate,
-    jurorMember1,
-    jurorMember2,
-    jurorMember3,
-  ] = await ethers.getSigners();
-
-  // FACTORIES
-  let factoryRoleManager = await ethers.getContractFactory("RoleManager");
-  let factoryUDAOVp = await ethers.getContractFactory("UDAOVp");
-  let factoryUDAOTimelockController = await ethers.getContractFactory(
-    "UDAOTimelockController"
-  );
-  let factoryUDAOCertificate = await ethers.getContractFactory(
-    "UDAOCertificate"
-  );
-  let factoryUDAO = await ethers.getContractFactory("UDAO");
-  let factoryUDAOStaker = await ethers.getContractFactory("UDAOStaker");
-  let factoryValidationManager = await ethers.getContractFactory(
-    "ValidationManager"
-  );
-  let factoryJurorManager = await ethers.getContractFactory("JurorManager");
-  let factoryUDAOContent = await ethers.getContractFactory("UDAOContent");
-  let factoryPlatformTreasury = await ethers.getContractFactory(
-    "PlatformTreasury"
-  );
-  let factoryUDAOGovernor = await ethers.getContractFactory("UDAOGovernor");
-  let factoryContractManager = await ethers.getContractFactory(
-    "ContractManager"
-  );
-
-  //DEPLOYMENTS
-  const contractUDAO = await factoryUDAO.deploy();
-
-  // Deploys PriceGetter
-  if (isDexRequired) {
-    const positionManager = await ethers.getContractAt(
-      NonFunbiblePositionABI,
-      "0xC36442b4a4522E871399CD717aBDD847Ab11FE88"
-    );
-    await helpers.setBalance(
-      backend.address,
-      ethers.utils.parseEther("1000000.0")
-    );
-    const WMATIC = await ethers.getContractAt(WMATIC_ABI, WMATICAddress);
-    await WMATIC.connect(backend).deposit({
-      value: ethers.utils.parseEther("1000.0"),
-    });
-
-    // call approve for tokens before adding a new pool
-
-    await WMATIC.connect(backend).approve(
-      positionManager.address,
-      ethers.utils.parseEther("99999999.0")
-    );
-
-    await contractUDAO
-      .connect(backend)
-      .approve(positionManager.address, ethers.utils.parseEther("9999999.0"));
-
-    const tx = await positionManager
-      .connect(backend)
-      .createAndInitializePoolIfNecessary(
-        WMATIC.address,
-        contractUDAO.address,
-        "3000",
-        "250541420775534450580036817218"
-      );
-    const result = await tx.wait();
-    const tx_2 = await positionManager
-      .connect(backend)
-      .mint([
-        WMATIC.address,
-        contractUDAO.address,
-        "3000",
-        "0",
-        "23040",
-        "950252822518485471",
-        "9999999999999999991268",
-        "0",
-        "9963392298778452810744",
-        backend.address,
-        "1678352028999",
-      ]);
-    const result_2 = await tx_2.wait();
-
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-    await helpers.time.increase(2);
-
-    // Price Getter End
-  }
-  let factoryPriceGetter = await ethers.getContractFactory("PriceGetter");
-  const contractRoleManager = await factoryRoleManager.deploy();
-  let contractPriceGetter;
-  if (isDexRequired) {
-    contractPriceGetter = await factoryPriceGetter.deploy(
-      "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-      contractUDAO.address,
-      "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
-      3000,
-      contractRoleManager.address
-    );
-  } else {
-    contractPriceGetter = { address: ethers.constants.AddressZero };
-  }
-
-  const contractUDAOCertificate = await factoryUDAOCertificate.deploy(
-    contractRoleManager.address
-  );
-  const contractUDAOContent = await factoryUDAOContent.deploy(
-    contractRoleManager.address
-  );
-  const contractValidationManager = await factoryValidationManager.deploy(
-    contractUDAOContent.address,
-    contractRoleManager.address
-  );
-  const contractJurorManager = await factoryJurorManager.deploy(
-    contractRoleManager.address,
-    contractUDAOContent.address,
-    contractValidationManager.address
-  );
-
-  const contractContractManager = await factoryContractManager.deploy(
-    contractValidationManager.address,
-    contractJurorManager.address,
-    contractUDAO.address,
-    contractUDAOContent.address,
-    contractRoleManager.address
-  );
-
-  const contractUDAOVp = await factoryUDAOVp.deploy(
-    contractRoleManager.address,
-    contractContractManager.address
-  );
-  const contractPlatformTreasury = await factoryPlatformTreasury.deploy(
-    contractContractManager.address,
-    contractRoleManager.address,
-    contractPriceGetter.address
-  );
-
-  const contractUDAOStaker = await factoryUDAOStaker.deploy(
-    contractPlatformTreasury.address,
-    contractRoleManager.address,
-    contractUDAOVp.address,
-    contractContractManager.address
-  );
-
-  const contractUDAOTimelockController =
-    await factoryUDAOTimelockController.deploy(1, [], [foundation.address]);
-  const contractUDAOGovernor = await factoryUDAOGovernor.deploy(
-    contractUDAOVp.address,
-    contractUDAOTimelockController.address,
-    contractUDAOStaker.address,
-    contractRoleManager.address
-  );
-  //POST DEPLOYMENT
-
-  // add proposer
-  const PROPOSER_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("PROPOSER_ROLE")
-  );
-  await contractUDAOTimelockController.grantRole(
-    PROPOSER_ROLE,
-    contractUDAOGovernor.address
-  );
-
-  const EXECUTOR_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("EXECUTOR_ROLE")
-  );
-  const JUROR_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("JUROR_ROLE")
-  );
-  await contractRoleManager.grantRole(JUROR_ROLE, jurorMember1.address);
-  await contractRoleManager.grantRole(JUROR_ROLE, jurorMember2.address);
-  await contractRoleManager.grantRole(JUROR_ROLE, jurorMember3.address);
-  // @dev Setting the zero address as the executor role will allow anyone to execute the proposal
-  await contractUDAOTimelockController.grantRole(
-    EXECUTOR_ROLE,
-    // zero address
-    "0x0000000000000000000000000000000000000000"
-  );
-
-  // grant roles
-  const TREASURY_CONTRACT = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("TREASURY_CONTRACT")
-  );
-  await contractRoleManager.grantRole(
-    TREASURY_CONTRACT,
-    contractPlatformTreasury.address
-  );
-  const BACKEND_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("BACKEND_ROLE")
-  );
-  await contractRoleManager.grantRole(BACKEND_ROLE, backend.address);
-  const JUROR_CONTRACT = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("JUROR_CONTRACT")
-  );
-  await contractRoleManager.grantRole(
-    JUROR_CONTRACT,
-    contractJurorManager.address
-  );
-  
-  //await contractUDAO.connect(backend).setApprovalForTimeLockController(contractUDAOTimelockController.address);
-  const FOUNDATION_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("FOUNDATION_ROLE")
-  );
-  await contractRoleManager.grantRole(FOUNDATION_ROLE, foundation.address);
-  const STAKING_CONTRACT = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("STAKING_CONTRACT")
-  );
-  await contractRoleManager.grantRole(
-    STAKING_CONTRACT,
-    contractUDAOStaker.address
-  );
-  const GOVERNANCE_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("GOVERNANCE_ROLE")
-  );
-  await contractRoleManager.grantRole(
-    GOVERNANCE_ROLE,
-    contractUDAOTimelockController.address
-  );
-  const GOVERNANCE_CONTRACT = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("GOVERNANCE_CONTRACT")
-  );
-  // TODO IS THIS NECESSARY?
-  await contractRoleManager.grantRole(
-    GOVERNANCE_CONTRACT,
-    contractUDAOGovernor.address
-  );
-  const VALIDATOR_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("VALIDATOR_ROLE")
-  );
-  await contractRoleManager.grantRole(VALIDATOR_ROLE, validator1.address);
-  await contractRoleManager.grantRole(VALIDATOR_ROLE, validator2.address);
-  await contractRoleManager.grantRole(VALIDATOR_ROLE, validator3.address);
-  await contractRoleManager.grantRole(VALIDATOR_ROLE, validator4.address);
-  await contractRoleManager.grantRole(VALIDATOR_ROLE, validator5.address);
-
-  const VALIDATION_MANAGER = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("VALIDATION_MANAGER")
-  );
-  await contractRoleManager.grantRole(
-    VALIDATION_MANAGER,
-    contractValidationManager.address
-  );
-  const DEFAULT_ADMIN_ROLE = ethers.utils.keccak256(
-    ethers.utils.toUtf8Bytes("DEFAULT_ADMIN_ROLE")
-  );
-  await contractRoleManager.grantRole(DEFAULT_ADMIN_ROLE, foundation.address);
-  await contractRoleManager.grantRole(
-    DEFAULT_ADMIN_ROLE,
-    contractUDAOTimelockController.address
-  );
-  // add missing contract addresses to the contract manager
-  await contractContractManager
-    .connect(backend)
-    .setAddressStaking(contractUDAOStaker.address);
-  await contractContractManager
-    .connect(backend)
-    .setPlatformTreasuryAddress(contractPlatformTreasury.address);
-  await contractContractManager
-    .connect(backend)
-    .setAddressUdaoVp(contractUDAOVp.address);
-  await contractJurorManager
-    .connect(backend)
-    .setContractManager(contractContractManager.address);
-  // add staking contract to udao-vp
-  await contractUDAOVp.connect(backend).updateAddresses();
-
-  return {
-    backend,
-    contentCreator,
-    contentBuyer1,
-    contentBuyer2,
-    contentBuyer3,
-    validatorCandidate,
-    validator1,
-    validator2,
-    validator3,
-    validator4,
-    validator5,
-    superValidatorCandidate,
-    superValidator,
-    foundation,
-    governanceCandidate,
-    governanceMember,
-    jurorCandidate,
-    jurorMember1,
-    jurorMember2,
-    jurorMember3,
-    contractUDAO,
-    contractRoleManager,
-    contractUDAOCertificate,
-    contractUDAOContent,
-    contractValidationManager,
-    contractPlatformTreasury,
-    contractUDAOVp,
-    contractUDAOStaker,
-    contractUDAOTimelockController,
-    contractUDAOGovernor,
-    contractJurorManager,
-    contractContractManager,
-  };
-}
 
 describe("Platform Treasury General", function () {
-  it("Should deploy", async function () {
-    const {
-      backend,
-      validatorCandidate,
-      validator,
-      superValidatorCandidate,
-      superValidator,
-      foundation,
-      governanceCandidate,
-      governanceMember,
-      jurorCandidate,
-      jurorMember1,
-      jurorMember2,
-      jurorMember3,
-      contractUDAO,
-      contractRoleManager,
-      contractUDAOCertificate,
-      contractUDAOContent,
-      contractValidationManager,
-      contractPlatformTreasury,
-      contractUDAOVp,
-      contractUDAOStaker,
-      contractUDAOTimelockController,
-      contractUDAOGovernor,
-      contractJurorManager,
-      contractContractManager,
-    } = await deploy();
-  });
   it("Should allow backend to set new governance treasury address", async function () {
     const {
       backend,
@@ -794,7 +427,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true, true);
     /// @dev Setup governance member
     await setupGovernanceMember(
       contractRoleManager,
@@ -1056,7 +689,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     /// @dev Setup governance member
     await setupGovernanceMember(
       contractRoleManager,
@@ -1331,7 +964,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     // Create content
     await createContent(
       contractRoleManager,
@@ -1429,7 +1062,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     // Create content
     await createContent(
       contractRoleManager,
@@ -1541,7 +1174,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     // Create content
     await createContent(
       contractRoleManager,
@@ -1715,7 +1348,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     // Create content
     await createContent(
       contractRoleManager,
@@ -1904,7 +1537,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true, true);
     /// Set KYC
     await contractRoleManager.setKYC(jurorMember1.address, true);
     await contractRoleManager.setKYC(jurorMember2.address, true);
@@ -1956,9 +1589,13 @@ describe("Platform Treasury General", function () {
     await contractJurorManager
       .connect(jurorMember2)
       .sendDisputeResult(disputeId, disputeResultOfJurorMember2);
-    await expect(contractJurorManager
-      .connect(jurorMember3)
-      .sendDisputeResult(disputeId, disputeResultOfJurorMember3)).to.emit(contractJurorManager, "DisputeEnded").withArgs(disputeId, disputeVerdict);
+    await expect(
+      contractJurorManager
+        .connect(jurorMember3)
+        .sendDisputeResult(disputeId, disputeResultOfJurorMember3)
+    )
+      .to.emit(contractJurorManager, "DisputeEnded")
+      .withArgs(disputeId, disputeVerdict);
 
     // Get the ID of the current distribution round
     const currentDistributionRound =
@@ -2092,7 +1729,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     // Create content
     await createContent(
       contractRoleManager,
@@ -2197,7 +1834,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
 
     /// Create content
     await createContent(
@@ -2304,7 +1941,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     /// Create content
     await createContent(
       contractRoleManager,
@@ -2404,7 +2041,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     /// Create content
     await createContent(
       contractRoleManager,
@@ -2483,7 +2120,7 @@ describe("Platform Treasury General", function () {
       contractUDAOGovernor,
       contractJurorManager,
       contractContractManager,
-    } = await deploy();
+    } = await deploy(false, true);
     /// Create content
     await createContent(
       contractRoleManager,
@@ -2600,7 +2237,7 @@ describe("Platform Treasury General", function () {
       contractJurorManager,
       contractContractManager,
       jurorContract,
-    } = await deploy();
+    } = await deploy(false, true);
     /// Create content
     await createContent(
       contractRoleManager,
@@ -2688,7 +2325,7 @@ describe("Platform Treasury General", function () {
       contractJurorManager,
       contractContractManager,
       JUROR_ROLE,
-    } = await deploy();
+    } = await deploy(false, true);
     /// Create content
     await createContent(
       contractRoleManager,
