e2b282d2216722c323069061e675e3111b89471d
mbd <bugrahan.duran@anywai.ch>
2023-09-08 17:10:03 +0300

    feat(presale): Created pre-sale contracts and functions

--- contracts/tokens/UDAO.sol
+++ contracts/tokens/UDAO.sol
@@ -3,9 +3,21 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../interfaces/ITokenSeller.sol";
 
 contract UDAO is ERC20 {
-    constructor() ERC20("UDAO", "UDAO") {
+    ITokenSeller public tokenSeller;
+    constructor(address _ITS) ERC20("UDAO", "UDAO") {
         _mint(msg.sender, 100000000 * 10**decimals());
+        tokenSeller = ITokenSeller(_ITS);
     }
+
+    /// @notice Allows users to withdraw their tokens from the token seller contract
+    function withdraw() external {
+        uint256 balance = tokenSeller.getBalance(msg.sender);
+        tokenSeller.resetBalance(msg.sender);
+        //_mint(msg.sender, balance);
+        transfer(msg.sender, balance); // if minted tokens are in this contract, use this line
+    }
+   
 }
