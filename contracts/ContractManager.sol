9ecf9aa981c00bbc7e3b41fc5d7f285062689dc5
mbd <bugrahan.duran@anywai.ch>
2023-09-12 10:08:58 +0300

    fix(role): Applied new role manager to the Contract Manager

--- contracts/ContractManager.sol
+++ contracts/ContractManager.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 /// @title Set contract addresses from a central location
 
-import "./RoleController.sol";
+import "./interfaces/IRoleManager.sol";
+import "./RoleNames.sol";
 
 pragma solidity ^0.8.0;
 
-contract ContractManager is RoleController {
+contract ContractManager is RoleNames {
+    IRoleManager roleManager;
+
     /// @dev Below should be set after the deployment
     address public StakingContractAddress; // staking contract
     address public PlatformTreasuryAddress; // platform treasury contract
@@ -15,7 +18,7 @@ contract ContractManager is RoleController {
     address public ISupVisAddress; // supervision interface address
     address public UdaoAddress; // udao token address
     address public UdaocAddress; // content token address
-    address public IrmAddress; // role manager interface address
+    address public RmAddress; // role manager interface address
 
     /// @dev unknown if before or after
     address public IPriceGetterAddress; // price getter interface address
@@ -24,60 +27,93 @@ contract ContractManager is RoleController {
         address _supAddress,
         address _udaoAddress,
         address _udaocAddress,
-        address _irmAddress
-    ) RoleController(_irmAddress) {
+        address _rmAddress
+    )  {
         // Set the initial addresses of contracts
         ISupVisAddress = _supAddress;
         UdaoAddress = _udaoAddress;
         UdaocAddress = _udaocAddress;
-        IrmAddress = _irmAddress;
+        RmAddress = _rmAddress;
+        roleManager = IRoleManager(_rmAddress);
     }
 
     function setPlatformTreasuryAddress(
         address _platformTreasuryAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set platform treasury address"
+        );
         PlatformTreasuryAddress = _platformTreasuryAddress;
     }
 
     function setAddressStaking(
         address _stakingAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set staking address"
+        );
         StakingContractAddress = _stakingAddress;
     }
 
     function setAddressUdaoVp(
         address _udaoVpAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set udao vp address"
+        );
         UdaoVpAddress = _udaoVpAddress;
     }
 
     function setAddressISupVisAddress(
         address _supAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set ISupVis address"
+        );
         ISupVisAddress = _supAddress;
     }
 
     function setAddressUdaoAddress(
         address _udaoAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external  {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set udao address"
+        );
         UdaoAddress = _udaoAddress;
     }
 
     function setAddressUdaocAddress(
         address _udaocAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set udaoc address"
+        );
         UdaocAddress = _udaocAddress;
     }
 
     function setAddressIrmAddress(
-        address _irmAddress
-    ) external onlyRole(BACKEND_ROLE) {
-        IrmAddress = _irmAddress;
+        address _rmAddress
+    ) external  {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set irm address"
+        );
+        RmAddress = _rmAddress;
     }
 
     function setAddressIPriceGetterAddress(
         address _priceGetterAddress
-    ) external onlyRole(BACKEND_ROLE) {
+    ) external {
+        require(
+            roleManager.hasRole(BACKEND_ROLE, msg.sender),
+            "Only backend can set price getter address"
+        );
         IPriceGetterAddress = _priceGetterAddress;
     }
 }
