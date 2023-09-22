e273e932a6156e386ae7c792bf8b717aad0f0921
mbd <bugrahan.duran@anywai.ch>
2023-09-22 21:38:39 +0300

    refactor(udao): Removed unnecessary parts from UDAO token

--- contracts/tokens/UDAO.sol
+++ contracts/tokens/UDAO.sol
@@ -3,21 +3,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../interfaces/ITokenSeller.sol";
 
 contract UDAO is ERC20 {
-    ITokenSeller public tokenSeller;
-    constructor(address _ITS) ERC20("UDAO", "UDAO") {
-        _mint(msg.sender, 100000000 * 10**decimals());
-        tokenSeller = ITokenSeller(_ITS);
+    constructor() ERC20("UDAO", "UDAO") {
+        _mint(msg.sender, 200000000 * 10 ** decimals());
     }
-
-    /// @notice Allows users to withdraw their tokens from the token seller contract
-    function withdraw() external {
-        uint256 balance = tokenSeller.getBalance(msg.sender);
-        tokenSeller.resetBalance(msg.sender);
-        //_mint(msg.sender, balance);
-        transfer(msg.sender, balance); // if minted tokens are in this contract, use this line
-    }
-   
 }
