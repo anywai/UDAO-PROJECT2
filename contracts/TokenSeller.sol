a8a911346834b40beff25275906d33bc65552b5c
mbd <bugrahan.duran@anywai.ch>
2023-09-26 11:23:27 +0300

    feat(seller): Added KYC to tokenseller

--- contracts/TokenSeller.sol
+++ contracts/TokenSeller.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenSeller is AccessControl {
+    bytes32 public constant RECORDER_ROLE = keccak256("RECORDER_ROLE");
     IERC20 public udaoToken;
     bool public tokenRelased = false;
 
@@ -15,12 +16,15 @@ contract TokenSeller is AccessControl {
     event BatchBalanceAdded(address[] users, uint256[] amounts);
     event BalanceReset(address user);
     event UDAOSet(address udaoToken);
+    event KYCStatusChanged(address user, bool status);
 
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(RECORDER_ROLE, msg.sender);
     }
     // user_address => balance
     mapping(address => uint256) public balances;
+    mapping(address => bool) public KYCList;
 
     /// @notice DEFAULT_ADMIN_ROLE sets the UDAO token address
     /// @param _udaoTokenAddress The address of the UDAO token
@@ -31,34 +35,48 @@ contract TokenSeller is AccessControl {
         emit UDAOSet(_udaoTokenAddress);
     }
 
+    /// @notice DEFAULT_ADMIN_ROLE resets the balance of the user
+    /// @param _user The address of the user
+    function grantRecorderRole(address _user) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(RECORDER_ROLE, _user);
+    }
     /// @notice DEFAULT_ADMIN_ROLE releases the tokens
     function releaseTokens() external onlyRole(DEFAULT_ADMIN_ROLE) {
         tokenRelased = true;
         emit TokensReleased(tokenRelased);
     }
 
-    /// @notice DEFAULT_ADMIN_ROLE creates records of the balances of the users
+    /// @notice RECORDER_ROLE sets the KYC status of the user
+    /// @param _user The address of the user
+    /// @param _status The KYC status of the user
+    function changeKYCStatus(address _user, bool _status) public onlyRole(RECORDER_ROLE) {
+        KYCList[_user] = _status;
+        emit KYCStatusChanged(_user, _status);
+    }
+    /// @notice RECORDER_ROLE creates records of the balances of the users
     /// @param _user The address of the user
     /// @param _amount The amount of tokens to be added to the user's balance
     function addBalance(
         address _user,
         uint256 _amount
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) external onlyRole(RECORDER_ROLE) {
         require(_amount > 0, "Amount must be greater than 0!");
         balances[_user] += _amount;
+        changeKYCStatus(_user, true);
         emit BalanceAdded(_user, _amount);
     }
 
-    /// @notice DEFAULT_ADMIN_ROLE creates batch records of the balances of the users
+    /// @notice RECORDER_ROLE creates batch records of the balances of the users
     /// @param _users The addresses of the users
     /// @param _amounts The amounts of tokens to be added to the user's balance
     function batchAddBalance(
         address[] calldata _users,
         uint256[] calldata _amounts
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) external onlyRole(RECORDER_ROLE) {
         require(_users.length == _amounts.length, "Arrays must be the same length!");
         for (uint256 i = 0; i < _users.length; i++) {
             balances[_users[i]] += _amounts[i];
+            changeKYCStatus(_users[i], true);
         }
         emit BatchBalanceAdded(_users, _amounts);
     }
@@ -72,6 +90,7 @@ contract TokenSeller is AccessControl {
     /// @notice Allows users to withdraw their tokens from the token seller contract
     function withdraw() external {
         require(tokenRelased, "Tokens are not released yet!");
+        require(KYCList[msg.sender], "You are not KYCed!");
         uint256 balance = balances[msg.sender];
         require(balance > 0, "You have no tokens to withdraw!");
         balances[msg.sender] = 0;
