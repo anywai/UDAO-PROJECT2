b363ed54b53e88aca3f30a6734e5923056735474
mbd <bugrahan.duran@anywai.ch>
2023-10-09 11:20:14 +0300

    refactor: Removed uniswap and price getter
    Co-authored-by: Batuhan Darcin <batuhan.darcin@anywai.ch>

--- contracts/interfaces/IPriceGetter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IPriceGetter {
-    function getUdaoOut(
-        uint128 amountIn, bytes32
-    ) external view returns (uint amountOut);
-}
