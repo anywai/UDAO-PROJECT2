3c615b40c3cc4f8fbbf08df312658b9becd18b8c
mbd <bugrahan.duran@anywai.ch>
2023-11-02 10:24:11 +0300

    fix(event): Converted NewContentCreated vars to indexed

--- contracts/tokens/UDAOC.sol
+++ contracts/tokens/UDAOC.sol
@@ -50,7 +50,7 @@ contract UDAOContent is
     // tokenId => partIds
     mapping(uint256 => uint256[]) public contentParts;
     /// @notice This event is triggered when a new content is created
-    event NewContentCreated(uint tokenId, address owner);
+    event NewContentCreated(uint indexed tokenId, address indexed owner);
     /// @notice This event is triggered when a new part is added to a content
     event newPartAdded(uint tokenId, uint newPartId, uint newPartPrice);
     /// @notice This event is triggered if the contract manager updates the addresses.
