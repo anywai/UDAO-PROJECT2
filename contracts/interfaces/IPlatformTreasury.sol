171100b665665c2bbe31e6357d9207422efbc78d
mbd <bugrahan.duran@anywai.ch>
2023-11-10 15:03:13 +0300

    refactor: Removed IPlatformTreasury, platform treasury interface

--- contracts/interfaces/IPlatformTreasury.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IPlatformTreasury {
-    function transferGovernanceRewards(address _to, uint _amount) external;
-
-    function forcedRefundJuror(uint256 _coachingId) external;
-}
