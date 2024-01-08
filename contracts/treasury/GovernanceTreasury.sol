fce28586c458c86a05663c771f8d3aef0dd5890e
MehmeDBDuran <bugrahan.duran@anywai.ch>
2024-01-08 11:41:46 +0300

    feat: Added old withdraw functions for governance, juror and validator
    Co-authored-by: batuhandrcn-anywai <batuhandrcn-anywai@users.noreply.github.com>

--- contracts/treasury/GovernanceTreasury.sol
+++ contracts/treasury/GovernanceTreasury.sol
@@ -9,11 +9,11 @@ contract GovernanceTreasury {
     // UDAO (ERC20) Token contract address
     IERC20 udao;
 
-    /// @notice Balance of jurors, its a pool for jurors transferred from platform treasury.
+    /// @notice Balance of jurors, its a pool for jurors.
     uint jurorBalance;
-    /// @notice Balance of validators, its a pool for validators transferred from platform treasury.
+    /// @notice Balance of validators, its a pool for validators.
     uint validatorBalance;
-    /// @notice Balance of governance, its a pool for governance transferred from platform treasury.
+    /// @notice Balance of governance, its a pool for governance.
     uint governanceBalance;
 
     /// @notice Address of the deployer of this dummy contract
@@ -42,6 +42,72 @@ contract GovernanceTreasury {
     /// @param _balance The balance to update.
     function governanceBalanceUpdate(uint _balance) external {
         governanceBalance += _balance;
+    }
+
+    /// @notice withdraws governance balance to governance treasury
+    /// TODO Bu fonksiyon gereksiz gibi. governanceTreasury aslında platformTreasury. Para burada birikiyor ve
+    /// transferGovernanceRewards fonksiyonu ile staking contract'a transfer ediliyor. Burada biriken parayı
+    /// governanceTreasury'e transfer etmek gereksiz.
+    function withdrawGovernance() external whenNotPaused {
+        require(
+            roleManager.hasRole(GOVERNANCE_ROLE, msg.sender),
+            "Only governance can withdraw"
+        );
+        uint withdrawableBalance = governanceBalance;
+        governanceBalance = 0; /// @dev zeroing before the actual withdraw
+        udao.transfer(governanceTreasury, withdrawableBalance);
+        emit GovernanceWithdrawn(withdrawableBalance);
+    }
+
+    /// @notice calculates validator earnings and withdraws calculated earning to validator's wallet
+    function withdrawValidator() external whenNotPaused {
+        require(
+            roleManager.hasRole(VALIDATOR_ROLE, msg.sender),
+            "Only validator can withdraw"
+        );
+        uint claimableRound = lastValidatorClaim[msg.sender];
+        uint withdrawableBalance = 0;
+        uint validatorScore = 0;
+        for (uint i = claimableRound; i < distributionRound; i++) {
+            validatorScore += ISupVis.getValidatorScore(
+                msg.sender,
+                claimableRound
+            );
+            withdrawableBalance += (payPerValidationScore[claimableRound] *
+                validatorScore);
+        }
+        lastValidatorClaim[msg.sender] = distributionRound;
+        udao.transfer(msg.sender, withdrawableBalance);
+        emit ValidatorWithdrawn(msg.sender, withdrawableBalance);
+    }
+
+    /// @notice calculates juror earnings and withdraws calculated earning to juror's wallet
+    function withdrawJuror() external whenNotPaused {
+        require(
+            roleManager.hasRole(JUROR_ROLE, msg.sender),
+            "Only juror can withdraw"
+        );
+        uint claimableRound = lastJurorClaim[msg.sender];
+        uint withdrawableBalance = 0;
+        uint jurorScore = 0;
+        for (uint i = claimableRound; i < distributionRound; i++) {
+            jurorScore += ISupVis.getJurorScore(msg.sender, claimableRound);
+            withdrawableBalance += (payPerJuror[claimableRound] * jurorScore);
+        }
+        lastJurorClaim[msg.sender] = distributionRound;
+        udao.transfer(msg.sender, withdrawableBalance);
+        emit JurorWithdrawn(msg.sender, withdrawableBalance);
+    }
+
+    function transferGovernanceRewards(
+        address _to,
+        uint _amount
+    ) external whenNotPaused {
+        require(
+            roleManager.hasRole(STAKING_CONTRACT, msg.sender),
+            "Only staking contract can transfer governance rewards"
+        );
+        udao.transfer(_to, _amount);
     }
 
     /// @notice Withdraws the balance of the treasury.
