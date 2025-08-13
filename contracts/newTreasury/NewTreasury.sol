d2d1ea2ff31ac481068facc8484bff6e3ff7a4ea
batuhandrcn-anywai <133757786+batuhandrcn-anywai@users.noreply.github.com>
2025-08-14 02:06:33 +0300

    refactor: Update OpenZeppelin contracts to version 5.0.0 and adjust imports accordingly

--- contracts/newTreasury/NewTreasury.sol
+++ contracts/newTreasury/NewTreasury.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.20;
 
 //import "@openzeppelin/contracts/access/AccessControl.sol"; //is AccessControl
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol"; // draft-EIP712.sol path’i eski. OZ v5+’te EIP712.sol.
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol"; // draft-EIP712.sol path’i eski. OZ v5+’te EIP712.sol.
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 using SafeERC20 for IERC20;
 
 interface IGovernanceTreasury {
